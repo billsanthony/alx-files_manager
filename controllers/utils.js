f61d2f27f7dad6d23868cd6d92f7db7599c7730a
billsanthony <anthonypaulakpan@gmail.com>
2023-12-06 00:20:01 +0000

    files

---
+++ controllers/utils.js
@@ -0,0 +1,12 @@
+// utils.js
+const checkRedisStatus = () => {
+  // Logic to check Redis status
+  // Return true if Redis is alive, false otherwise
+};
+
+const checkDBStatus = () => {
+  // Logic to check DB status
+  // Return true if DB is alive, false otherwise
+};
+
+module.exports = { checkRedisStatus, checkDBStatus };
